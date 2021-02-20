5f2c8e068a9a6a7400317f0b9d8472380e8372a0
Ilya <dnamix1@gmail.com>
2021-02-20 13:08:51 +0300

    main bootstrap function and app.module.ts with TypeORM and GraphQL schema gen configured

---
+++ uber-eats-backend/src/app.module.ts
@@ -0,0 +1,62 @@
+import * as Joi from 'joi';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+import { User } from './users/entities/user.entity';
+import { JwtAuthModule } from './jwt-auth/jwt-auth.module';
+import { JwtMiddleware } from './jwt-auth/jwt-auth.middleware';
+import { AuthModule } from './auth/auth.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
+      ignoreEnvFile: process.env.NODE_ENV === 'prod',
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string().valid('dev', 'prod').required(),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.string().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_DATABASE: Joi.string().required(),
+        SECRET_KEY_TOKEN: Joi.string().required(),
+      }),
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: 'postgres',
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      synchronize: process.env.NODE_ENV !== 'prod',
+      logging: process.env.NODE_ENV !== 'prod',
+      entities: [User],
+    }),
+    GraphQLModule.forRoot({
+      autoSchemaFile: true,
+      context: ({ req, res }) => ({ req, res, user: req['user'] }),
+    }),
+    UsersModule,
+    JwtAuthModule.forRoot({ privateKey: process.env.SECRET_KEY_TOKEN }),
+    AuthModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(JwtMiddleware).forRoutes({
+      path: '/graphql',
+      method: RequestMethod.ALL,
+    });
+  }
+}
