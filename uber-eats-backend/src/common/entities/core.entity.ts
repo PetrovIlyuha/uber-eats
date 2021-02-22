24b81d5ae389e3761b901605d3faac21e65d71b2
Ilya <dnamix1@gmail.com>
2021-02-22 13:04:52 +0300

    core entity creating graphql schema foundation for many derivative entities and creating postgres table records with typeorm bindings

---
+++ uber-eats-backend/src/common/entities/core.entity.ts
@@ -0,0 +1,17 @@
+import { Field, ObjectType } from '@nestjs/graphql';
+import { CreateDateColumn, PrimaryGeneratedColumn } from 'typeorm';
+
+@ObjectType()
+export class CoreEntity {
+  @PrimaryGeneratedColumn()
+  @Field((type) => Number)
+  id: number;
+
+  @CreateDateColumn()
+  @Field((type) => Date)
+  createdAt: Date;
+
+  @CreateDateColumn()
+  @Field((type) => Date)
+  updatedAt: Date;
+}
