b666d0513db617b1fea252ee74436c9d87f71787
Ilya <dnamix1@gmail.com>
2021-03-01 18:04:14 +0300

    add jwt-auth-middleware checking the token presence in the request headers

---
+++ uber-eats-backend/src/jwt-auth/jwt-auth.middleware.ts
@@ -0,0 +1,28 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { NextFunction, Request, Response } from 'express';
+import { UserService } from 'src/users/users.service';
+import { JwtAuthService } from './jwt-auth.service';
+
+@Injectable()
+export class JwtMiddleware implements NestMiddleware {
+  constructor(
+    private readonly jwtService: JwtAuthService,
+    private readonly userService: UserService,
+  ) {}
+  async use(req: Request, res: Response, next: NextFunction) {
+    if ('jwt-token' in req.headers) {
+      const token = req.headers['jwt-token'];
+      try {
+        const decodedToken = this.jwtService.verify(token.toString());
+        if (
+          typeof decodedToken === 'object' &&
+          decodedToken.hasOwnProperty('id')
+        ) {
+          const user = await this.userService.findById(decodedToken['id']);
+          req['user'] = user;
+        }
+      } catch (error) {}
+    }
+    next();
+  }
+}
