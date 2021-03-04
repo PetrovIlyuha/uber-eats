ce20d3d1d0d05ae63693a42a76607ce80f264d96
Ilya <dnamix1@gmail.com>
2021-03-04 15:06:10 +0300

    add guard check of metadata passed to resolvers with Reflector

--- uber-eats-backend/src/auth/auth.guard.ts
+++ uber-eats-backend/src/auth/auth.guard.ts
@@ -1,15 +1,28 @@
+import { AllowedRoles } from './role.decorator';
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { GqlExecutionContext } from '@nestjs/graphql';
+import { User } from 'src/users/entities/user.entity';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
+  constructor(private readonly reflector: Reflector) {}
   canActivate(context: ExecutionContext) {
-    const gqlContext = GqlExecutionContext.create(context).getContext();
-    const user = gqlContext['user'];
-    if (!user) {
-      return false;
-    } else {
+    const roles = this.reflector.get<AllowedRoles>(
+      'roles',
+      context.getHandler(),
+    );
+    if (!roles) {
       return true;
     }
+    const gqlContext = GqlExecutionContext.create(context).getContext();
+    const user: User = gqlContext['user'];
+    if (!user) {
+      return false;
+    }
+    if (roles.includes('Anybody')) {
+      return true;
+    }
+    return roles.includes(user.role);
   }
 }
