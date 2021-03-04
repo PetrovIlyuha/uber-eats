ed11a26d377a37e14a262d45432826f0a8f08076
Ilya <dnamix1@gmail.com>
2021-03-04 15:06:51 +0300

    add Authentication guard to providers of Auth module

--- uber-eats-backend/src/auth/auth.module.ts
+++ uber-eats-backend/src/auth/auth.module.ts
@@ -1,4 +1,13 @@
+import { AuthGuard } from 'src/auth/auth.guard';
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
 
-@Module({})
+@Module({
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+  ],
+})
 export class AuthModule {}
