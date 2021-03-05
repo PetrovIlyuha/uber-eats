fd0d164d6d5da9e70244b3fd76e8210fcee0b338
Ilya <dnamix1@gmail.com>
2021-03-05 13:39:54 +0300

    add jwt-auth service

---
+++ uber-eats-backend/src/jwt-auth/jwt-auth.service.ts
@@ -0,0 +1,19 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { JwtModuleOptions } from './jwt.interfaces';
+import * as jwt from 'jsonwebtoken';
+import { CONFIG_OPTIONS } from 'src/common/common.constants';
+
+@Injectable()
+export class JwtAuthService {
+  constructor(
+    @Inject(CONFIG_OPTIONS) private readonly options: JwtModuleOptions,
+  ) {}
+  sign(userId: number): string {
+    return jwt.sign({ id: userId }, this.options.privateKey, {
+      expiresIn: '20d',
+    });
+  }
+  verify(token: string) {
+    return jwt.verify(token, this.options.privateKey);
+  }
+}
