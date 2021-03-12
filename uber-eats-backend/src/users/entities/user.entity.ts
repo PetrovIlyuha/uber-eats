e7dfa3e19dda98170c2934fa8892b6a12c357a8a
Ilya <dnamix1@gmail.com>
2021-03-12 17:12:19 +0300

    adding user graphql schema and typeorm entity model for User

---
+++ uber-eats-backend/src/users/entities/user.entity.ts
@@ -0,0 +1,80 @@
+import { Order } from './../../orders/entities/order.entity';
+import {
+  Field,
+  InputType,
+  ObjectType,
+  registerEnumType,
+} from '@nestjs/graphql';
+import { CoreEntity } from 'src/common/entities/core.entity';
+import { BeforeInsert, BeforeUpdate, Column, Entity, OneToMany } from 'typeorm';
+import * as bcrypt from 'bcrypt';
+import { InternalServerErrorException } from '@nestjs/common';
+import { IsBoolean, IsEmail, IsEnum, IsString } from 'class-validator';
+import { Restaurant } from 'src/restaurants/entities/restaurant.entity';
+
+export enum UserRole {
+  Owner = 'Owner',
+  Client = 'Client',
+  Delivery = 'Delivery',
+}
+
+registerEnumType(UserRole, { name: 'UserRole' });
+
+@InputType('UserInputType', { isAbstract: true })
+@ObjectType()
+@Entity()
+export class User extends CoreEntity {
+  @Column()
+  @Field((returns) => String)
+  @IsEmail()
+  email: string;
+
+  @Column({ select: false })
+  @Field((returns) => String)
+  @IsString()
+  password: string;
+
+  @Column({ type: 'enum', enum: UserRole })
+  @IsEnum(UserRole)
+  @Field((returns) => UserRole)
+  role: UserRole;
+
+  @Column({ default: false })
+  @Field((_) => Boolean)
+  @IsBoolean()
+  emailVerified: boolean;
+
+  @Field(() => [Restaurant])
+  @OneToMany((type) => Restaurant, (restaurant) => restaurant.owner)
+  restaurants: Restaurant[];
+
+  @Field(() => [Order])
+  @OneToMany((type) => Order, (order) => order.customer)
+  orders: Order[];
+
+  @Field(() => [Order])
+  @OneToMany((type) => Order, (order) => order.driver)
+  deliveries: Order[];
+
+  @BeforeInsert()
+  @BeforeUpdate()
+  async hashPassword(): Promise<void> {
+    if (this.password) {
+      try {
+        this.password = await bcrypt.hash(this.password, 10);
+      } catch (error) {
+        console.log('hashpass error', error);
+        throw new InternalServerErrorException();
+      }
+    }
+  }
+
+  async checkGivenPassword(password: string): Promise<boolean> {
+    try {
+      return await bcrypt.compare(password, this.password);
+    } catch (error) {
+      console.error(error);
+      throw new InternalServerErrorException();
+    }
+  }
+}
