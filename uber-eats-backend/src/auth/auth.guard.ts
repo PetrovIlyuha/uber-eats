50469c53ad99a8a09a4b40b07ad3923f9768a457
Ilya <dnamix1@gmail.com>
2021-03-16 18:07:59 +0300

    updating auth guard in accordance with the need to be able to provide authentication functionality both for http and websockets

--- uber-eats-backend/src/auth/auth.guard.ts
+++ uber-eats-backend/src/auth/auth.guard.ts
@@ -1,13 +1,19 @@
+import { JwtAuthService } from './../jwt-auth/jwt-auth.service';
 import { AllowedRoles } from './role.decorator';
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { GqlExecutionContext } from '@nestjs/graphql';
 import { User } from 'src/users/entities/user.entity';
+import { UserService } from 'src/users/users.service';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private readonly reflector: Reflector) {}
-  canActivate(context: ExecutionContext) {
+  constructor(
+    private readonly reflector: Reflector,
+    private readonly userService: UserService,
+    private readonly jwtService: JwtAuthService,
+  ) {}
+  async canActivate(context: ExecutionContext) {
     const roles = this.reflector.get<AllowedRoles>(
       'roles',
       context.getHandler(),
@@ -16,13 +22,24 @@ export class AuthGuard implements CanActivate {
       return true;
     }
     const gqlContext = GqlExecutionContext.create(context).getContext();
-    const user: User = gqlContext['user'];
-    if (!user) {
-      return false;
+    const token = gqlContext.token;
+    if (token) {
+      const decodedToken = this.jwtService.verify(token.toString());
+      if (
+        typeof decodedToken === 'object' &&
+        decodedToken.hasOwnProperty('id')
+      ) {
+        const { user } = await this.userService.findById(decodedToken['id']);
+        if (!user) {
+          return false;
+        }
+        gqlContext['user'] = user;
+        if (roles.includes('Anybody')) {
+          return true;
+        }
+        return roles.includes(user.role);
+      }
     }
-    if (roles.includes('Anybody')) {
-      return true;
-    }
-    return roles.includes(user.role);
+    return false;
   }
 }
