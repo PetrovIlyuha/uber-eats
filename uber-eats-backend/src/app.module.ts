d6164b0bd8bddf2ebcd4f3687823835c2d80a1a2
Ilya <dnamix1@gmail.com>
2021-03-17 17:12:37 +0300

    adding critical imports in varios modules

--- uber-eats-backend/src/app.module.ts
+++ uber-eats-backend/src/app.module.ts
@@ -23,6 +23,7 @@ import { Dish } from './restaurants/entities/dish.entity';
 import { OrdersModule } from './orders/orders.module';
 import { Order } from './orders/entities/order.entity';
 import { OrderItem } from './orders/entities/order-item.entity';
+import { CommonModule } from './common/common.module';
 
 @Module({
   imports: [
@@ -73,6 +74,7 @@ import { OrderItem } from './orders/entities/order-item.entity';
       },
     }),
     UsersModule,
+    CommonModule,
     RestaurantsModule,
     JwtAuthModule.forRoot({ privateKey: process.env.SECRET_KEY_TOKEN }),
     AuthModule,
--- uber-eats-backend/src/auth/auth.module.ts
+++ uber-eats-backend/src/auth/auth.module.ts
@@ -1,8 +1,10 @@
+import { UsersModule } from './../users/users.module';
 import { AuthGuard } from 'src/auth/auth.guard';
 import { Module } from '@nestjs/common';
 import { APP_GUARD } from '@nestjs/core';
 
 @Module({
+  imports: [UsersModule],
   providers: [
     {
       provide: APP_GUARD,
