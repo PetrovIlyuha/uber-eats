8fa8eaf0abfb7ea3b7ac60382e5e7d352bc89e8d
Ilya <dnamix1@gmail.com>
2021-03-22 16:43:45 +0300

    add routes for client role router in logged in app state

--- uber-eats-frontend/src/routers/logged-in-router.tsx
+++ uber-eats-frontend/src/routers/logged-in-router.tsx
@@ -1,14 +1,59 @@
 import React from 'react'
+import gql from 'graphql-tag'
+import { useQuery } from '@apollo/client'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+import { AiOutlineLoading3Quarters } from 'react-icons/ai'
+
 import { isLoggedInVar } from '../apollo'
+import { meQuery } from '../__api_schema_typed__/meQuery'
+import Restaurants from '../pages/client/Restaurants'
+import Page404 from '../pages/404'
+import Header from '../components/Header'
+import useCurrentUser from '../hooks/useCurrentUser'
+
+const ClientRouter = () => (
+  <Switch>
+    <Route path="/" exact>
+      <Restaurants />
+    </Route>
+    <Route component={Page404} exact path="*" />
+  </Switch>
+)
+
+
+
+const CURRENT_USER_QUERY = gql`
+  query meQuery {
+    me {
+      id
+      email
+      role
+      emailVerified
+    }
+  }
+`
 
 export const LoggedInRouter = () => {
   const logOut = () => {
     isLoggedInVar(false)
   }
+  const { data, loading, error } = useCurrentUser();
+  console.log(data?.me.role)
+  if (!data || loading || error) {
+    return (
+      <div className="h-screen flex justify-center items-center">
+        <div className="flex flex-col items-center">
+          <h2 className="mb-10 text-2xl font-medium tracking-wider">Wait. Just Watch</h2>
+          <AiOutlineLoading3Quarters className="animate-spin" size={100} color="green" />
+        </div>
+      </div>
+    )
+  }
   return (
-    <div>
-      <h1>Logged In</h1>
-      <button onClick={logOut} className="btn bg-contain bg-red-300">Click to Logout</button>
-    </div>
+    <Router>
+      <Header/>
+      {data.me.role === 'Client' && <ClientRouter/>}
+    </Router >
   )
 }
