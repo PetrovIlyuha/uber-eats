525f316a3bb9c3730f4bcb7cdcface95dace1bcf
Ilya <dnamix1@gmail.com>
2021-03-26 14:51:28 +0300

    main restaurants page with categories, restaurant grid and pagination

--- uber-eats-frontend/src/pages/client/Restaurants.tsx
+++ uber-eats-frontend/src/pages/client/Restaurants.tsx
@@ -1,7 +1,11 @@
 import { useMutation, useQuery } from '@apollo/client'
 import gql from 'graphql-tag'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Helmet } from 'react-helmet-async'
+import CategoriesIconsBar from '../../components/categories/CategoriesIconsBar'
+import Restaurant from '../../components/restaurants/Restaurant'
+import Container from '../../components/reusable/Container'
+import RestaurantsPaginator from '../../components/reusable/RestaurantsPaginator'
 import { restaurantsPageQuery, restaurantsPageQueryVariables } from '../../__api_schema_typed__/restaurantsPageQuery'
 
 const RESTAURANTS_QUERY = gql`
@@ -40,24 +44,62 @@ const RESTAURANTS_QUERY = gql`
 
 
 const Restaurants = () => {
+  const [page, setPage] = useState(1)
   const [getRestaurantsPage, { data, loading, error }] =
     useMutation<restaurantsPageQuery, restaurantsPageQueryVariables>(
       RESTAURANTS_QUERY,
       {
-        variables: { input: { page: 1 } }
+        variables: { input: { page } }
       })
 
   useEffect(() => {
     getRestaurantsPage()
-  }, [])
+  }, [page])
+  console.log(data);
+  const changeToNextPage = () => {
+    setPage(page => page + 1)
+  }
+  const changeToPrevPage = () => {
+    setPage(page => page - 1)
+  }
 
   return (
-    <div className="mt-12">
+    <>
       <Helmet>
         <title>Restaurants | Grabs Eaters</title>
       </Helmet>
-      <h1>Restaurants Screen</h1>
-    </div>
+      <form className="bg-gray-800 w-full py-28 flex items-center justify-center">
+        <input type="search" placeholder="Search for restaurant..." className="input py-2 w-3/12" />
+      </form>
+      {!loading && data?.allCategories && (
+        <Container>
+          <CategoriesIconsBar data={data?.allCategories?.categories} />
+          <RestaurantsPaginator
+            data={data}
+            page={page}
+            changeToNextPage={changeToNextPage}
+            changeToPrevPage={changeToPrevPage}
+          />
+          <div className="mt-10 grid lg:grid-cols-3 md:grid-cols-2 gap-x-5 gap-y-10">
+            {data?.restaurants?.results?.map(restaurant => (
+              <Restaurant
+                key={restaurant.id}
+                id={String(restaurant.id)}
+                coverImage={restaurant?.coverImage}
+                name={restaurant?.name}
+                categoryName={restaurant?.category?.name}
+              />
+            ))}
+          </div>
+          <RestaurantsPaginator
+            data={data}
+            page={page}
+            changeToNextPage={changeToNextPage}
+            changeToPrevPage={changeToPrevPage}
+          />
+        </Container>
+      )}
+    </>
   )
 }
 
