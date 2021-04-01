072e868b4a05e5b4585729fee0ec20d9f8d48169
Ilya <dnamix1@gmail.com>
2021-04-01 17:16:24 +0300

    added AWS S3 image files upload module and controller

--- uber-eats-backend/src/restaurants/dtos/owner-restaurants.dto.ts
+++ uber-eats-backend/src/restaurants/dtos/owner-restaurants.dto.ts
@@ -1,11 +1,9 @@
 import { BasicResponse } from './../../common/dtos/mutation-response.dto';
-import { Field, ObjectType } from "@nestjs/graphql";
+import { Field, ObjectType } from '@nestjs/graphql';
 import { Restaurant } from '../entities/restaurant.entity';
-
-
 
 @ObjectType()
 export class OwnerRestaurantsOutput extends BasicResponse {
-  @Field(type => [Restaurant])
-  restaurants?: Restaurant[]
+  @Field((type) => [Restaurant])
+  restaurants?: Restaurant[];
 }
---
+++ uber-eats-backend/src/uploads/uploads.controller.ts
@@ -0,0 +1,44 @@
+import {
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import * as AWS from 'aws-sdk';
+
+//AKIAYEYCZJOZ6QI47FXO
+// G8DCxLJf50U3Gfvs7Beu7Nf5r/AWLzhEAeQylDQx
+
+const BUCKET_NAME = 'grabs-eaters-uploads';
+
+@Controller('uploads')
+export class UploadsController {
+  @Post('')
+  @UseInterceptors(FileInterceptor('file'))
+  async uploadFile(@UploadedFile() file) {
+    AWS.config.update({
+      credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_KEY,
+      },
+    });
+    try {
+      const objectName = String(`${Date.now()}${file.originalname}`);
+      const upload = await new AWS.S3()
+        .putObject({
+          Bucket: BUCKET_NAME,
+          Body: file.buffer,
+          Key: objectName,
+          ContentType: 'image/jpeg',
+          ACL: 'public-read',
+        })
+        .promise();
+      const fileUrl = `https://${BUCKET_NAME}.s3.amazonaws.com/${objectName}`;
+      return { imageUrl: fileUrl };
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
+  }
+}
