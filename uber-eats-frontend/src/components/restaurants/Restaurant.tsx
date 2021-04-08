c95cd6b8a27f0143db744459cf5ab6f1bf58ccd6
Ilya <dnamix1@gmail.com>
2021-04-08 10:22:09 +0300

    add intersection observer to single restaurant card component

--- uber-eats-frontend/src/components/restaurants/Restaurant.tsx
+++ uber-eats-frontend/src/components/restaurants/Restaurant.tsx
@@ -1,19 +1,40 @@
-import React from 'react'
+import { motion } from 'framer-motion'
+import { useInView } from 'react-intersection-observer'
+import React, { useEffect } from 'react'
 
 interface RestaurantProps {
-  id: string;
   coverImage: string;
   name: string;
   categoryName?: string;
 }
 
-const Restaurant: React.FC<RestaurantProps> = ({ id, coverImage, name, categoryName }) => {
+const Restaurant: React.FC<RestaurantProps> = ({ coverImage, name, categoryName }) => {
+  const [ref, inView] = useInView({
+    threshold: 0.35
+  });
+
+  useEffect(() => {
+    console.log(inView)
+  }, [inView])
+
   return (
-    <div className="flex flex-col">
-      <div className="py-28 bg-cover mb-3" style={{ backgroundImage: `url(${coverImage})` }}></div>
+    <motion.div
+      ref={ref}
+      animate={{ opacity: inView ? 1 : 0, x: inView ? -30 : 0 }}
+      transition={{ duration: 0.4, delay: 0.3, ease: "backOut" }}
+      className="flex flex-col"
+    >
+      <motion.div
+        className="py-28 bg-cover mb-3"
+        initial={{ opacity: 0 }}
+        whileHover={{ opacity: 1, transition: { duration: 0.1 } }}
+        animate={{ opacity: 0.8 }}
+        transition={{ duration: 0.3, delay: 0.5 }}
+        style={{ backgroundImage: `url(${coverImage})` }}>
+      </motion.div>
       <h3 className="font-medium">{name}</h3>
       <span className="font-semibold border-t-2 border-gray-300 w-full text-xs py-2 mt-3">Kitchen: {categoryName}</span>
-    </div>
+    </motion.div>
   )
 }
 
